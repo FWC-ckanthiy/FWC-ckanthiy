faf0de7762c7acc266d8ebf48eb926c16c7e1f6c
FWC-ckanthiy <67050763@kmitl.ac.th>
2026-09-16 23:11:58 +0700

    Complete Cell03 Exercise 01

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,32 @@
+const balloon = document.getElementById("balloon");
+
+let size = 200;
+let colorIndex = 0;
+
+const colors = ["red", "green", "blue"];
+
+balloon.addEventListener("click", function() {
+    size += 10;
+
+    if (size > 420) {
+        size = 200;
+    }
+
+    colorIndex = (colorIndex + 1) % 3;
+
+    balloon.style.width = size + "px";
+    balloon.style.height = size + "px";
+    balloon.style.backgroundColor = colors[colorIndex];
+});
+
+balloon.addEventListener("mouseleave", function() {
+    if (size > 200) {
+        size -= 5;
+    }
+
+    colorIndex = (colorIndex - 1 + 3) % 3;
+
+    balloon.style.width = size + "px";
+    balloon.style.height = size + "px";
+    balloon.style.backgroundColor = colors[colorIndex];
+});
